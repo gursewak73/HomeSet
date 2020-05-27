4080e9c7150990419e3cd65a9f5a7ddbd38af3b9
gursewaksingh <gursewak73@gmail.com>
2020-05-27 21:54:45 +0530

    Implement navigation graph

--- app/src/main/java/com/wallpaper/homeset/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/wallpaper/homeset/ui/activity/HomeActivity.kt
@@ -2,11 +2,12 @@ package com.wallpaper.homeset.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wallpaper.homeset.R
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.home_activity)
     }
 }
--- app/src/main/java/com/wallpaper/homeset/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/wallpaper/homeset/ui/fragment/HomeFragment.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.wallpaper.homeset.R
-import com.wallpaper.homeset.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var mRoot: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mRoot = DataBindingUtil.inflate(layoutInflater, R.layout.home_fragment, container, false)
-        return mRoot.root
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
